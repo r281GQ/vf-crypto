214daacff909326cde7975c6cb536bcc97d1a110
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 09:29:10 +0200

    add comments to cache

--- src/store/reducers/cache.js
+++ src/store/reducers/cache.js
@@ -1,5 +1,38 @@
 import { REJECTED, RESOLVED, START_REQUEST } from "./../constants";
 
+/**
+ * The core idea of the app is to have a central cache.
+ *
+ * This is a very naive imitation of Apollo's InMemory cache.
+ *
+ * We can have entries, all of them has their internal state
+ * and of course their data.
+ *
+ * With this structure later we can link two entities together.
+ *
+ * A cache entry is defined by a unique id. That is set up initially by the
+ * "START_REQUEST" action.
+ *
+ * This checks if the entry is there. If not, it creates it with the default values.
+ * If it is, just overwrites some state.
+ *
+ * These values are:
+ *
+ * * data: the data that we want to store.
+ * * loading: loading state that can change over time. For example when we poll or refetch
+ *            this can be toggled.
+ * * initialLoading: This will be only true when we try to fetch for the first time. The aim to make two
+ *                   different loading indicator is to use the cache without loading state and still be
+ *                   able to refetch in the background.
+ * * error: whatever error the fetch request or any other business logic might throw. This is deleted
+ *          when a subsequent fetch succeeds.
+ * * fetchMoreLoading, hasMore: just to support pagination.
+ * * lastFetched: timestamp when was the last fetch.
+ *
+ *
+ * Based on the outcome, the cache can have a "RESOLVED" or a "REJECTED" action. They both update the store
+ * accordingly.
+ */
 const cache = (prevState = {}, action) => {
   /**
    * Start request when there is no entry in the cache.
