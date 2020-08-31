264ec0c068dfdaa305ed1639d2a829009fa5c01a
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 08:12:52 +0200

    add sagas

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
 import reducers from "./reducers";
+import rootSaga from "./sagas";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [
+  sagaMiddleware,
+  process.env.NODE_ENV === "development" && logger,
+].filter(Boolean);
 
 const store = createStore(reducers, applyMiddleware(...middleWares));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
---
+++ src/store/sagas/index.js
@@ -0,0 +1,67 @@
+import { all, call, take, race, put } from "redux-saga/effects";
+
+const delay = (seconds) => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000)
+  );
+};
+
+const mockFetch = (data = "some value") => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(data);
+    }, 1000)
+  );
+};
+
+function* handleTopTenRequest(currency) {
+  yield put({
+    type: "START_REQUEST",
+    payload: { cacheEntry: `top10_${currency}` },
+  });
+
+  try {
+    const result = yield mockFetch(currency);
+
+    yield put({
+      type: "RESOLVED",
+      payload: { cacheEntry: `top10_${currency}`, data: result },
+    });
+  } catch (error) {
+    yield put({
+      type: "REJECTED",
+      payload: { cacheEntry: `top10_${currency}`, error },
+    });
+  }
+}
+
+function* pollTopTen(currency) {
+  while (true) {
+    try {
+      yield call(handleTopTenRequest, currency);
+
+      yield call(delay, 60);
+    } catch (error) {
+      yield put({ type: "STOP_POLLING", error });
+    }
+  }
+}
+
+function* topTenPollWorker() {
+  while (true) {
+    const action = yield take("START_POLLING_TOP_TEN");
+
+    yield race([
+      call(pollTopTen, action.payload.currency),
+      take("STOP_POLLING"),
+    ]);
+  }
+}
+
+function* rootSaga() {
+  yield all([topTenPollWorker()]);
+}
+
+export default rootSaga;
