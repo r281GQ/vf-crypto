6ecf99d08e538dd26d80fdae049ad11c1431ecb9
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 07:55:50 +0200

    connect TopTen to redux store

--- src/routes/topTen/index.js
+++ src/routes/topTen/index.js
@@ -1,5 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { setSelectedCurrency } from "./../../store/actions";
+
 const TopTen = (props) => {
+  React.useEffect(() => {
+    props.setSelectedCurrency("EUR");
+  }, []);
+
   return "topten";
 };
 
-export default TopTen;
+export default connect((state) => ({ state }), { setSelectedCurrency })(TopTen);
