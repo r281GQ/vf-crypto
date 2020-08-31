360fd323b3a4023bdceffa5f83f76fa13897b4d8
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 12:52:07 +0200

    add layout elements to topten

--- src/routes/topTen/index.js
+++ src/routes/topTen/index.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { css, jsx } from "@emotion/core";
 import { connect } from "react-redux";
+
+import Footer from "./../../components/footer";
+import Header from "./../../components/header";
+import Layout from "./../../components/layout";
+import Main from "./../../components/main";
+import Spinner from "./../../components/spinner";
 
 import {
   setSelectedCurrency,
@@ -20,7 +27,26 @@ const TopTen = (props) => {
     };
   }, []);
 
-  return "topten";
+  return (
+    <Layout>
+      <Header>
+        <div
+          css={css`
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+          `}
+        >
+          <div>Crypto</div>
+          <div>setSelectedCurrency</div>
+        </div>
+      </Header>
+      <Main>
+        <Spinner />
+      </Main>
+      <Footer>footer</Footer>
+    </Layout>
+  );
 };
 
 export default connect((state) => ({ state }), {
