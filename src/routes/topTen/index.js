01a71cd2671bf06300ef9e21bf773e5fb7d113d4
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 13:22:30 +0200

    clean up effects and reselect in topten

--- src/routes/topTen/index.js
+++ src/routes/topTen/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { css, jsx } from "@emotion/core";
 import { connect } from "react-redux";
+import { createSelector } from "reselect";
 
 import Footer from "./../../components/footer";
 import Header from "./../../components/header";
@@ -15,20 +16,35 @@ import {
 } from "./../../store/actions";
 
 import CurrencySelector from "./../../views/currencySelector";
+import UpdateNotifier from "./../../views/updateNotifier";
 
 /** @jsx jsx */
 const TopTen = (props) => {
-  React.useEffect(() => {
-    props.setSelectedCurrency("EUR");
-  }, []);
+  const { startPollingTopTen, stopPolling, selectedCurrency } = props;
 
+  /**
+   * This dispatches the action to start polling.
+   *
+   * The API endpoint changes as we select a new currency.
+   *
+   * We only need to watch "selectedCurrency" to avoid the pitfalls of
+   * closures, but included the functions so we can have a proper linting experience
+   */
   React.useEffect(() => {
-    props.startPollingTopTen("EUR");
+    startPollingTopTen(selectedCurrency);
 
     return () => {
-      props.stopPolling();
+      stopPolling();
     };
-  }, []);
+  }, [startPollingTopTen, stopPolling, selectedCurrency]);
+
+  const currencies =
+    props.currencies && props.currencies.data
+      ? props.currencies.data.cryptos
+      : [];
+
+  const lastFetched = props.currencies && props.currencies.lastFetched;
+  const initialLoading = props.currencies && props.currencies.initialLoading;
 
   return (
     <Layout>
@@ -47,12 +63,42 @@ const TopTen = (props) => {
       <Main>
         <Spinner />
       </Main>
-      <Footer>footer</Footer>
+      <Footer>
+        <UpdateNotifier key={lastFetched} lastFetched={lastFetched} />
+      </Footer>
     </Layout>
   );
 };
 
-export default connect((state) => ({ state }), {
+/**
+ * Memoized selector to get the proper cache entry and avoid unwanted updates.
+ */
+const top10CacheSelector = createSelector(
+  (state) => state.cache,
+  (state) => state.selectedCurrency,
+  (cache, currency) => {
+    return cache[`top10_${currency}`];
+  }
+);
+
+/**
+ * Maybe a bit overkill, but does not hurt. If later the logic gets more complicated, reselect always comes good.
+ */
+const selectedCurrencySelector = createSelector(
+  (state) => state.selectedCurrency,
+  (selectedCurrency) => {
+    return selectedCurrency;
+  }
+);
+
+const mapStateToProps = (state) => {
+  return {
+    currencies: top10CacheSelector(state),
+    selectedCurrency: selectedCurrencySelector(state),
+  };
+};
+
+export default connect(mapStateToProps, {
   setSelectedCurrency,
   startPollingTopTen,
   stopPolling,
