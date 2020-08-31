b5a81b6ef81fdcb3dd97e19fe7a68a4ce05f6ec0
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 08:13:10 +0200

    add polling to topten component

--- src/routes/topTen/index.js
+++ src/routes/topTen/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { setSelectedCurrency } from "./../../store/actions";
+import {
+  setSelectedCurrency,
+  startPollingTopTen,
+  stopPolling,
+} from "./../../store/actions";
 
 const TopTen = (props) => {
   React.useEffect(() => {
     props.setSelectedCurrency("EUR");
   }, []);
 
+  React.useEffect(() => {
+    props.startPollingTopTen("EUR");
+
+    return () => {
+      props.stopPolling();
+    };
+  }, []);
+
   return "topten";
 };
 
-export default connect((state) => ({ state }), { setSelectedCurrency })(TopTen);
+export default connect((state) => ({ state }), {
+  setSelectedCurrency,
+  startPollingTopTen,
+  stopPolling,
+})(TopTen);
