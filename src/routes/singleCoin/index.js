e9a05e364ff6b98a69fcebc008ad8b86d694188b
Endre Vegh <endre.vegh@yahoo.com>
2020-08-31 13:55:40 +0200

    connect indivudaul route to coin

--- src/routes/singleCoin/index.js
+++ src/routes/singleCoin/index.js
@@ -1,16 +1,44 @@
+import React from "react";
 import { css, jsx } from "@emotion/core";
+import { connect } from "react-redux";
 
 import Footer from "../../components/footer";
 import Header from "./../../components/header";
 import Layout from "./../../components/layout";
 import Main from "./../../components/main";
 
+import { startPollingIndividual, stopPolling } from "./../../store/actions";
+
 import theme from "../../theme";
 
 import CurrencySelector from "./../../views/currencySelector";
 
 /** @jsx jsx */
-const SingleCoin = () => {
+const SingleCoin = (props) => {
+  const name = props.match.params.coin;
+
+  const { selectedCurrency, startPollingIndividual, stopPolling } = props;
+
+  React.useEffect(() => {
+    startPollingIndividual(name, selectedCurrency);
+
+    return () => {
+      stopPolling();
+    };
+  }, [name, selectedCurrency, startPollingIndividual, stopPolling]);
+
+  const image = props.crypto && props.crypto.data && props.crypto.data.image;
+  const initialLoading =
+    props.crypto && props.crypto.initialLoading && props.crypto.initialLoading;
+  const lastFetched =
+    props.crypto && props.crypto.data && props.crypto.lastFetched;
+  const marketCap =
+    props.crypto && props.crypto.data && props.crypto.data.marketCap;
+  const price = props.crypto && props.crypto.data && props.crypto.data.price;
+  const supply = props.crypto && props.crypto.data && props.crypto.data.supply;
+  const twentyFourHourVolume =
+    props.crypto && props.crypto.data && props.crypto.data.twentyFourHourVolume;
+
   return (
     <Layout>
       <Header>
@@ -37,4 +65,17 @@ const SingleCoin = () => {
   );
 };
 
-export default SingleCoin;
+export default connect(
+  (state, ownProp) => {
+    const name = ownProp.match.params.coin;
+
+    return {
+      selectedCurrency: state.selectedCurrency,
+      crypto: state.cache[`individual_${name}_to_${state.selectedCurrency}`],
+    };
+  },
+  {
+    startPollingIndividual,
+    stopPolling,
+  }
+)(SingleCoin);
